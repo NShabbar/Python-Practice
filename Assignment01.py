ba011704e8894a273e563677338871b624f829e7
Nshabbar <nid.shabbar@gmail.com>
2025-05-09 15:23:50 -0700

    Attempting to add input validation.

--- Assignment01.py
+++ Assignment01.py
@@ -3,9 +3,24 @@
 
 import math
 
+# A function to check if user inputs are valid
+def number_valid(number):
+    if number == "":
+        print("You did not enter a value.")
+        output = float(input("Please enter a desired length for a cube: "))
+        number_valid(output)
+    elif type(number) != (int | float):
+        print("You did not enter a valid number. Please try again.")
+        output = float(input("Please enter a desired length for a cube: "))
+        number_valid(output)
+    else:
+        return output
+
 # Since the cube with be equal on all sides, only one measurement is needed.
 # The formula for the volume of a cube is length(width)(height)
-cube_length = float(input("Please enter a desired length for a cube: "))
+cube_length = number_valid(number = (input("Please enter a desired length for a cube: ")))
+
+
 
 #To find the volume of a cone, the formula is: (radius^2(height/3)(pi))
 cone_height = float(input("Please enter a desired height for the cone: "))
