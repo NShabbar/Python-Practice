266eb250965e145bf9881b5082d44f22bd623c4f
Nshabbar <nid.shabbar@gmail.com>
2025-05-09 13:38:03 -0700

    Began revisiting old school assignments and improving upon them. In Assignment01, need to add checks for valid user inputs.

---
+++ Assignment00.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+
+#input allows a user to input values or strings to be held in the variable
+x = int(input("Enter a number to be subtracted from: "))
+y = int(input("Enter a number to subtract from X: "))
+
+#f before the string allows a person to input variables within the {}
+print(f"The subtraction of X and Y is {x-y}.") 
---
+++ Assignment01.py
@@ -0,0 +1,22 @@
+# A program designed to calculate the volume of a cube and a cone 
+# based on a user's inputs
+
+import math
+
+# Since the cube with be equal on all sides, only one measurement is needed.
+# The formula for the volume of a cube is length(width)(height)
+cube_length = float(input("Please enter a desired length for a cube: "))
+
+#To find the volume of a cone, the formula is: (radius^2(height/3)(pi))
+cone_height = float(input("Please enter a desired height for the cone: "))
+cone_radius = float(input("Please enter a desired radius for the cone: "))
+
+# The ** syntax allows for exponent math. Similarly math.pow(x, y) 
+# where x is the number being raised to y power.
+
+# cube_volume = cube_length ** 3
+cube_volume = math.pow(cube_length, 3)
+cone_volume = math.pow(cone_radius, 2) * (cone_height/3) * math.pi 
+
+print(f"The volume of the cube is: {cube_volume}.")
+print(f"The volume of the cone is: {cone_volume}.")
